cbbd133cc1895cf10df936b5ac37e544018aad9c
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-05-12 00:45:50 +0300

    feat: transfer messages between client & server

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const socket = io('http://localhost:3000')
 socket.on('connect-chat', data => {
   console.log(data);
 })
+
+var sendButton = document.getElementById('sendBtn');
+var message    = document.getElementById('messageContent');
+
+sendButton.addEventListener('click', e => {
+  e.preventDefault();
+  
+  // send it to the server
+  socket.emit('message-channel', message.value);
+  message.value = '';
+})
--- server.js
+++ server.js
@@ -8,4 +8,12 @@ const io = require("socket.io")(3000, {
 
 io.on("connection", (socket) => {
   socket.emit("connect-chat", "test_message");
+  
+  // handle the incoming message
+  socket.on('message-channel', message => {
+    // log the message just for testing purposes.
+    console.log(message);
+  })
 });
+
+
